be76eb5063c3f64019c4ea874adfe15ce386be67
Dev-Moretti <daviabelmoretti1@gmail.com>
2023-12-19 18:20:46 -0300

    final da tela de click

---
+++ Campo-Minado/EndGame.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Campo_Minado
+{
+    /// <summary>
+    /// Lógica interna para EndGame.xaml
+    /// </summary>
+    public partial class EndGame : Window
+    {
+        public EndGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
